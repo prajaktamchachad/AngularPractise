848162029346b412ff362d261b5e59aa08112433
prajaktamchachad <prajaktamchachad@gmail.com>
2024-01-13 19:40:10 -0500

    mvvm

---
+++ app.js
@@ -0,0 +1,30 @@
+(function () {
+    'use strict'; 
+
+    angular.module('NameCalculator', [])
+    .controller('NameCalculatorController', function($scope)
+    {
+    $scope.name = "";
+    $scope.totalValue = 0;
+
+    $scope.displayNumeric = function()
+    {
+        var totalNumericValue = calculateNumericForString($scope.name ); //get the total 
+        $scope.totalValue = totalNumericValue;
+    };
+
+   function calculateNumericForString(string)
+   {
+    var totalStringValue = 0;
+    for(var i=0; i<string.length ;i++)
+    {
+        totalStringValue +=string.charCodeAt(i);
+    }
+    return totalStringValue;
+   }
+{
+
+}
+
+    });
+}) ();
